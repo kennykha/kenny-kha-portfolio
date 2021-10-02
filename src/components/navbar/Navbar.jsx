01c0e17179a5a3cd68a45796b4c64ebd0e360776
Kenny Kha <69876050+kennykha@users.noreply.github.com>
2021-10-02 15:41:07 -0700

    Adding initial logo for return to intro. Styling container for logo and email

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,20 @@
 import "./navbar.scss";
+import { Mail } from "@mui/icons-material"
 
 export default function Navbar() {
     return (
         <div className='navbar'>
-            
+            <div className="wrapper">
+                <div className="left">
+                    <a href="#intro" className="logo">KK</a>
+                    <div className="mailContainer">
+                        <Mail className="mailIcon"/>
+                        <span>kennykha@gmail.com</span>
+                    </div>
+                </div>
+                <div className="right">
+                </div>
+            </div>
         </div>
     )
 }
