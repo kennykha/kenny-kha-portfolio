8c34b0b2bb93cbf85a1cefbb6b64e058edf36f24
Kenny Kha <69876050+kennykha@users.noreply.github.com>
2021-10-02 16:02:58 -0700

    Adding animation transition for navbar and menu icon

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Intro from "./components/intro/Intro";
 import Projects from "./components/projects/Projects";
 import Contact from "./components/contact/Contact";
 import "./app.scss";
+import { useState } from "react";
 
 function App() {
+  const [menuOpen, handleMenuOpen] = useState(false);
   return (
     <div className="app">
-      <Navbar />
+      <Navbar menuOpen={menuOpen} handleMenuOpen={handleMenuOpen} />
       <div className="sections">
         <Intro />
         <Projects />
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,9 @@
 import "./navbar.scss";
 import { Mail } from "@mui/icons-material"
 
-export default function Navbar() {
+export default function Navbar({menuOpen, handleMenuOpen}) {
     return (
-        <div className='navbar'>
+        <div className={'navbar ' + (menuOpen && "active")}>
             <div className="wrapper">
                 <div className="left">
                     <a href="#intro" className="logo">KK</a>
@@ -13,6 +13,11 @@ export default function Navbar() {
                     </div>
                 </div>
                 <div className="right">
+                    <div className="menu" onClick={() => handleMenuOpen(!menuOpen)}>
+                        <span className="line1"></span>
+                        <span className="line2"></span>
+                        <span className="line3"></span>
+                    </div>
                 </div>
             </div>
         </div>
