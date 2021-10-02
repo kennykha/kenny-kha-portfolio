52548947830f63416845d6aef7bb5e51012acad9
Kenny Kha <69876050+kennykha@users.noreply.github.com>
2021-10-02 15:03:32 -0700

    Adding intro, projects, contact and navbar components. Initial styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import Navbar from "./components/navbar/Navbar";
+import Intro from "./components/intro/Intro";
+import Projects from "./components/projects/Projects";
+import Contact from "./components/contact/Contact";
+import "./app.scss";
+
 function App() {
   return (
-    <div className="App">
-      <div>Testing</div>
+    <div className="app">
+      <Navbar />
+      <div className="sections">
+        <Intro />
+        <Projects />
+        <Contact />
+      </div>
     </div>
   );
 }
