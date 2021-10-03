07e69bc02137b7646048aeb4fd629abf4958fc25
Kenny Kha <69876050+kennykha@users.noreply.github.com>
2021-10-02 18:00:36 -0700

    Adding basic layout of intro component with image and description

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -3,6 +3,18 @@ import "./intro.scss";
 export default function Intro() {
     return (
         <div className="intro" id="intro">
+            <div className="left">
+                <div className="imageContainer">
+                    <img src="/assets/kenny.jpeg" alt="kenny" />
+                </div>
+            </div>
+            <div className="right">
+                <div className="introRightContainer">
+                    <h2>Hello, my name is</h2>
+                    <h1 className="name">Kenny Kha</h1>
+                    <h3>Full-stack Javascript Developer</h3>
+                </div>
+            </div>
         </div>
     )
 }
