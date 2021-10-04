007a32061b6f2a32fca20460091b8f3b12627162
Kenny Kha <69876050+kennykha@users.noreply.github.com>
2021-10-03 18:13:50 -0700

    Adding projects to project section

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,8 +1,27 @@
 import "./projects.scss";
+import GitHub from '@mui/icons-material/GitHub';
 
 export default function Projects() {
     return (
         <div className="projects" id="projects">
+            <div className="name">Projects</div>
+            <div className="carousel">
+                <div className="container">
+                    <div className="project">
+                        <div className="left">
+                            <h2>Adidas: E-Commerce Site</h2>
+                            <p>Javascript, React, Node.js, Express.js, AWS EC2, MySQL</p>
+                            <a href="https://github.com/kennykha/Adidas-Ratings-and-Reviews" target="_blank" rel="noreferrer" className="gitHub">
+                                <div>GitHub</div>
+                                <GitHub className="icon"/>
+                            </a>
+                        </div>
+                        <div className="right">
+                            <img src="assets/Adidas.png" alt="adidas" />
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
